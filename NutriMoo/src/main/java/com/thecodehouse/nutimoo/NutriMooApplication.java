abbc5f5d08bb0d6824f4220d2b5435c84367acbe
Julio Leite <julioleite93d@gmail.com>
2023-11-08 18:22:07 -0300

    Inicialização do projeto spring boot

---
+++ NutriMoo/src/main/java/com/thecodehouse/nutimoo/NutriMooApplication.java
@@ -0,0 +1,13 @@
+package com.thecodehouse.nutimoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriMooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriMooApplication.class, args);
+	}
+
+}
---
+++ NutriMoo/src/test/java/com/thecodehouse/nutimoo/NutriMooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecodehouse.nutimoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriMooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
